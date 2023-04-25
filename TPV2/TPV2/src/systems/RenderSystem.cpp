ce58a6639054756cf6d6063573cc22d0d83d015d
JoseMorenob <100274773+JoseMorenob@users.noreply.github.com>
2023-04-25 13:46:31 +0200

    funciona?

--- TPV2/TPV2/src/systems/RenderSystem.cpp
+++ TPV2/TPV2/src/systems/RenderSystem.cpp
@@ -26,10 +26,15 @@ void RenderSystem::receive(const Message& m) {
 		onRoundStart();
 		break;
 	case PLAYER1_WIN:
-		state_ = 3;
+		if (mngr_->getComponent<Health>(mngr_->getHandler(_HDLR_FIGHTER))->GetHealth() < 1) {
+			state_ = 3;
+		}
+	
 		break;
 	case PLAYER2_WIN:
-		state_ = 3;
+		if (mngr_->getComponent<Health>(mngr_->getHandler(_HDLR_NETFIGHTER_2))->GetHealth() < 1) {
+			state_ = 3;
+		}
 		break;
 	}
 }
